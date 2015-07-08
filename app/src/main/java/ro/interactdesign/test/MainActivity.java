7d38f62f3d59145a793bddf33bb1bda80ec2fff0
Mokokus <marius.teodor.mm@gmail.com>
2015-07-08 14:05:48 +0300

    second commit test

--- app/src/main/java/ro/interactdesign/test/MainActivity.java
+++ app/src/main/java/ro/interactdesign/test/MainActivity.java
@@ -7,11 +7,14 @@ import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
+    public TestClass clasa = new TestClass();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        clasa.setText("text");
     }
 
     @Override
